2b584f496ad650f770c40f9b9abece52f2f86831
Zander Work <zander@zanderwork.com>
2022-02-20 20:03:15 -0500

    things kinda sorta maybe work

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # synapse-triage
 Synapse Rapid Power-Up for Hatching Triage
+
+## Install
+
+TBD
+
+## Test
+
+_Warning: `sample.zip` is extracted in memory and contains a live malware sample used for testing this functionality._
+
+You must have a Tria.ge public cloud API key to run the tests. You can put the key in `$SYNAPSE_TRIAGE_APIKEY` for the test suite, or pass it to stdin when prompted.
+
+```
+$ SYNAPSE_TRIAGE_APIKEY=asdf python -m pytest test_synapse_triage.py
+```
---
+++ test_synapse_triage.py
@@ -0,0 +1,89 @@
+import binascii
+import hashlib
+import os
+import logging
+import zipfile
+
+import synapse.axon as s_axon
+import synapse.common as s_common
+import synapse.tests.utils as s_test
+import synapse.tools.genpkg as s_genpkg
+
+logger = logging.getLogger(__name__)
+
+dirname = os.path.dirname(__file__)
+pkgproto = s_common.genpath(dirname, "synapse-triage.yaml")
+
+def get_mal_bytes():
+    with zipfile.ZipFile("sample.zip") as zf:
+        with zf.open("sample", "r", b"infected") as f:
+            return f.read()
+
+class SynapseTriageTest(s_test.SynTest):
+
+    def get_api_key(self):
+        key = os.getenv("SYNAPSE_TRIAGE_APIKEY")
+        if key is not None:
+            return key
+        
+        return input("Please enter your Hatching Triage Public Cloud API Key: ")
+
+    async def test_synapse_triage(self):
+        # this test suite requires internet access
+        self.skipIfNoInternet()
+
+        # get API key
+        api_key = self.get_api_key()
+
+        async with self.getTestCore() as core:
+            # upload malware sample to test axon
+            mal_bytes = get_mal_bytes()
+            mal_sha256 = hashlib.sha256(mal_bytes).hexdigest()
+
+            async with await core.axon.upload() as fd:
+                await fd.write(mal_bytes)
+                _, axon_sha256 = await fd.save()
+                self.eq(binascii.hexlify(axon_sha256).decode(), mal_sha256)
+
+            # create file:bytes node in the cortex
+            nodes = await core.nodes("[ file:bytes=$hash :name=$name ]", opts={"vars": {"hash": mal_sha256, "name": "raccon_test_boi"}})
+            self.len(1, nodes)
+            
+            # install package
+            await s_genpkg.main((pkgproto, "--push", f"cell://{core.dirn}"))
+
+            # set the api key
+            msgs = await core.stormlist("triage.setup.apikey --self $key", opts={"vars": {"key": api_key}})
+            self.stormIsInPrint("for the current user", msgs)
+            
+            msgs = await core.stormlist("triage.setup.apikey $key", opts={"vars": {"key": api_key}})
+            self.stormIsInPrint("for all users", msgs)
+
+            # submit the sample to triage
+            msgs = await core.stormlist("file:bytes:sha256=$hash | triage.submit", opts={"vars": {"hash": mal_sha256}})
+            self.stormIsInPrint("Hatching Triage (sample ID: ", msgs)
+
+
+
+
+
+
+
+            return
+
+            # some test APIs to simplify async generators and such...
+            msgs = await core.stormlist("acme.hello.print --show-prefix --prefix VISI \"hello world!\"")
+            self.stormIsInPrint("VISI hello world!", msgs)
+
+            nodes = await core.nodes("[ inet:email=visi@vertex.link ] | acme.hello.autotag")
+            self.len(1, nodes)
+            self.true(nodes[0].tags.get("acme.hello") is not None)
+
+            # check that the meta source node got created and linked...
+            nodes = await core.nodes("meta:source:name=\"ACME Hello World\" -(seen)> *")
+            self.len(1, nodes)
+            self.eq(nodes[0].ndef, ("inet:email", "visi@vertex.link"))
+
+            # check that the description is in the auto generated `--help` output
+            msgs = await core.stormlist("acme.hello.print --help")
+            self.stormIsInPrint("Print some text", msgs)
